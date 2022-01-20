1add9c3dd71c432f25c334581fe1e9212581a7bc
Coderbro <mo.abbaszadi@gmail.com>
2022-01-20 21:31:08 +0000

    ascii-art-web

--- server.go
+++ server.go
@@ -12,7 +12,7 @@ func main() {
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Printf("Starting server at port 8080\n") // https://blog.logrocket.com/creating-a-web-server-with-golang/
+	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
@@ -33,14 +33,23 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-    if err := r.ParseForm(); err != nil {
-        fmt.Fprintf(w, "ParseForm() err: %v", err)
-        return
-    }
-    fmt.Fprintf(w, "POST request successful")
-    name := r.FormValue("name")
-    address := r.FormValue("address")
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
 
-    fmt.Fprintf(w, "Name = %s\n", name)
-    fmt.Fprintf(w, "Address = %s\n", address)
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
+
+/*
+https://blog.logrocket.com/creating-a-web-server-with-golang/
+https://go.dev/doc/articles/wiki/
+
+http://localhost:8080
+http://localhost:8080/form
+http://localhost:8080/hello
+*/
